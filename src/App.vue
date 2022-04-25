74f8d1c8bc973321122a8d08d990e79bfa36ca07
yonghui <yonghuihuang412@gmail.com>
2022-04-25 22:23:37 +0200

    Adding 2 lines scrolling row to show food info, adding spliter component

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,59 @@
 <template>
   <div class="header">
     <img class="logo" alt="projet logo" src="./assets/logo.png">
-    <span class="title">slogan</span>
+    <span class="title">Un passage fluide et sans surprise pour un repas serein</span>
+  </div>
+  <spliter class="spliter"></spliter>
+  <div class="scroll">
+    <div v-for="item in tabList" :key="item">
+      <div class="container">
+        <img :src="item.url" height="150" width="200">
+<!--        <img src="./assets/fish.jpeg">-->
+        <div class="text-container">
+          <span class="text-left">{{ item.name }}</span>
+          <span class="text-right">{{ item.amount }}</span>
+        </div>
+      </div>
+    </div>
   </div>
 <!--  <HelloWorld msg="Welcome to Gestion de la cantine"/>-->
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import Spliter from "@/components/spliter";
 
-// export default {
-//   name: 'App',
-//   components: {
-//     HelloWorld
-//   }
-// }
+
+export default {
+  // eslint-disable-next-line vue/no-unused-components
+  components: {Spliter},
+  data() {
+    return {
+      tabList: [
+        { id: "01", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "02", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "03", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "04", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "05", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "06", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "07", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "08", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "09", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "10", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "11", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "12", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "13", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "06", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "07", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "08", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "09", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "10", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "11", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "12", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+        { id: "13", name: 'fish', amount: "5", url: require('@/assets/fish.jpeg')},
+      ]
+    }
+  }
+}
 </script>
 
 <style>
@@ -33,17 +72,45 @@
   margin-left: 20px;
   margin-top: 20px;
   overflow: hidden;
+  display: block;
+  text-align: center;
 }
 
 .logo {
+  position: absolute;
+  left: 30px;
+  top: 15px;
   height: 50px;
   width: 50px;
 }
 
 .title {
-  text-align: center;
-  position: absolute;
-  left: 45%;
-  font-size: 35px;
+  font-size: 28px;
+}
+
+.spliter {
+  height: 20px;
+  margin-top: 10px;
+}
+
+.scroll {
+  display: flex;
+  overflow-x: auto;
+  margin-top: 20px;
+  height: 400px;
+  flex-direction: column;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  align-content: flex-start;
+}
+
+.container {
+  width: 220px;
+}
+
+.text-container {
+  display: flex;
+  width: 200px;
+  justify-content: space-between;
 }
 </style>
---
+++ src/components/spliter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="spliter"></div>
+</template>
+
+<script>
+export default {
+// eslint-disable-next-line vue/multi-word-component-names
+  name: 'spliter',
+}
+</script>
+
+<style scoped>
+.spliter {
+  width: 100%;
+  height: 2px;
+  background-color: grey;
+}
+</style>
