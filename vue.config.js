fd2f7bb6faf4e73c45c3e8ffadbfb29b81015c4c
ycao <yuancaocc@163.com>
2022-05-15 10:29:08 +0200

    photo not found bug

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  configureWebpack:{
+    resolve:{
+      alias:{
+        'assets':'@/assets',
+        'views':'@/views',
+      }
+    },
+    module: {
+      rules: [
+        {
+          test: /\.mjs$/,
+          include: /node_modules/,
+          type: "javascript/auto"
+        },
+      ]
+    }
+  },
 })
